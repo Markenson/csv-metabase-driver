730051c3b8e0a1a752b9ffea6f8560c2a6cea317
markenson <markensonfranca@gmail.com>
2019-10-20 13:30:43 -0300

    Opening repo worldwide

--- csv/project.clj
+++ csv/project.clj
@@ -1,8 +1,16 @@
 (defproject metabase/csv-driver "1.0.0-SNAPSHOT"
   :min-lein-version "2.5.0"
 
+  :repositories [
+  ["csvjdbc4metabase" {
+    :url "https://maven.pkg.github.com/Markenson/csvjdbc4metabase",
+	:password "672fece31cd8c83673b2ca364236f6fd71c8e2fb", ;public read-only token
+	:username "markenson"
+	}
+	]
+   ]
   :dependencies
-  [[net.sourceforge.csvjdbc/csvjdbc "1.0.0-metabase"]]
+  [[net.sourceforge.csvjdbc/csvjdbc "1.0.10-metabase"]]
 
   :profiles
   {:provided
